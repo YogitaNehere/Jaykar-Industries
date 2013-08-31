b0aa3c7444cdd4ad27dca53e452e2161fb1dd43c
snehakumavat <snehakame@gmail.com>
2013-08-31 12:37:13 +0530

    yogita

---
+++ inventory/purches.php
@@ -0,0 +1,172 @@
+<?php
+include("../session/session.php");
+
+include("../include/database.php");
+$per_page = 19;
+
+$sql = "select * from purches";
+$rsd = mysql_query($sql);
+$count = mysql_num_rows($rsd);
+$pages = ceil($count/$per_page);
+
+?>
+<html>
+<head>
+<title>Jaykar Industries</title>
+<link rel="stylesheet" href="../styles2.css" type="text/css" />
+<link rel="stylesheet" type="text/css" href="../css/style.css" media="screen" />
+<link rel="shortcut icon" href="images/favicon.ico">
+<script type="text/javascript" src="../js/jquery.min.js"></script>
+<script type="text/javascript" src="js/addrow.js"></script>
+<script type="text/javascript" src="custom.js"></script>
+<script type="text/javascript" src="../js/jquery.js"></script>
+<script type="text/javascript" src="../js/slider.js"></script>
+<script type="text/javascript" src="../js/superfish.js"></script>
+<script type="text/javascript" src="../js/toword.js"></script>
+<script>
+var total = 0;
+function getValues() {
+var qty = 0;
+var rate = 0;
+var obj = document.getElementsByTagName("input");
+      for(var i=0; i<obj.length; i++){
+         if(obj[i].name == "qnt[]")
+		 {
+			 var qty = obj[i].value;
+		 }
+         if(obj[i].name == "r[]")
+		 {
+			 var rate = obj[i].value;
+		 }
+         if(obj[i].name == "value[]")
+		 {
+          		if(qty > 0 && rate > 0)
+				{
+					obj[i].value = qty*rate;
+					total+=(obj[i].value*1);
+				}
+				else
+				{
+					obj[i].value = 0;
+				    total+=(obj[i].value*1);
+				}
+          }
+		 }
+		 var tax =document.getElementById("stax").value;
+		 var vat =document.getElementById("vat").value;
+		 var add=total*1;
+		 add+=(tax*1);
+		  add+=(vat*1);		 
+        document.getElementById("total").value = add;
+		var words = toWords(add);
+		document.getElementById("word").innerHTML=words;
+        total=0;
+}
+function getQuant()
+{
+	$q1="select * from sub_sale where product=";
+}
+</script>
+	<script type="text/javascript">
+	
+	$(document).ready(function(){
+		
+	//Display Loading Image
+	function Display_Load()
+	{
+	    $("#loading").fadeIn(900,0);
+	
+	}
+	//Hide Loading Image
+	function Hide_Load()
+	{
+		$("#loading").fadeOut('slow');
+	};
+	
+
+    //Default Starting Page Results
+   
+	$("#pagination li:first").css({'color' : '#FF0084'}).css({'border' : 'none'});
+	
+	Display_Load();
+	
+	$("#content").load("purchespagination.php?page=1", Hide_Load());
+
+
+
+	//Pagination Click
+	$("#pagination li").click(function(){
+			
+		Display_Load();
+		
+		//CSS Styles
+		$("#pagination li")
+		.css({'color' : '#0063DC'});
+		
+		$(this)
+		.css({'color' : '#FF0084'})
+		.css({'border' : 'none'});
+
+		//Loading Data
+		var pageNum = this.id;
+		
+		$("#content").load("purchespagination.php?page=" + pageNum, Hide_Load());
+	});
+	
+	
+});
+	</script>
+</head>
+<body>
+<div id="container">
+<div id="sub-header">
+		<?php
+		   include("include/p_header.php");
+		?>
+        
+       	<table class="emp_tab">
+        <tr class="search_res">
+        <td class="info" align="center">
+        <span>Purches Details </span>
+        </td>
+        </tr>
+        </table>
+        <div class="popupbox_small" id="popuprel">
+		<div id="intabdiv">
+        
+        </div>
+		</div>
+       		<div id="loading" ></div>
+			<div id="content" ></div>
+            
+        	<table width="800px">
+			<tr><Td>
+			<ul id="pagination">
+				<?php
+				//Show page links
+				for($i=1; $i<=$pages; $i++)
+				{
+					echo '<li id="'.$i.'">'.$i.'</li>';
+				}
+				?>
+	</ul>	
+	</Td></tr></table>
+
+                </div>
+                              
+               
+  				</div>
+                
+                </div>
+                
+ <div id="fade"></div>
+       
+  	<div class="clear"></div>
+    </div>
+</div>
+ <div id="footer">
+     <div class="clear"></div> 
+    </div>
+    </div>
+</body>
+</html>
